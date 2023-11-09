36a610f3edddf96380fbceb6fdc6239d92ca3f42
Dimo Mitev <dimosoftuni@gmail.com>
2023-11-09 19:25:39 +0200

    Add our first test

---
+++ tests/ui.test.js
@@ -0,0 +1,9 @@
+const { expect, test } = require("@playwright/test");
+
+test("Verify All Books link is visible ", async ( {page} ) => {
+    await page.goto("http://localhost:3000");
+    await page.waitForSelector("#site-header > nav");
+    const allBooksLink = await page.$('a[href="/catalog"]');
+    const isAllBooksLinkVisible = await allBooksLink.isVisible();
+    expect(isAllBooksLinkVisible).toBe(true);
+});
